047fbd3ec33da166cc0f568203fcee06e208e6fe
Edward <edwarduw@gmail.com>
2014-08-02 12:00:21 -0600

    Se agregan Clases de abstracfactory

---
+++ WebServer/CarguerosWebServer/CarguerosWebServer/Services/CDAbstractFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarguerosWebServer.Services.ConcreteRepository;
+
+namespace CarguerosWebServer.Services
+{
+    public class CDAbstractFactory
+    {
+        //variables
+        private static CDAbstractFactory instance;
+
+        //methods
+        private CDAbstractFactory() { }
+
+        public static CDAbstractFactory Instance{
+      get 
+      {
+         if (instance == null)
+         {
+             instance = new CDAbstractFactory();
+         }
+         return instance;
+      }
+   }
+
+        
+
+
+
+    }
+}
