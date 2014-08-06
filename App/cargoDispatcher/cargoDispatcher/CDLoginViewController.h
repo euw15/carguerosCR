34a488b4f333ab5af2b87a81060113b89ee45d12
Edward <edwarduw@gmail.com>
2014-08-06 17:29:37 -0600

    Primeros view controllers agregados

---
+++ App/cargoDispatcher/cargoDispatcher/CDLoginViewController.h
@@ -0,0 +1,19 @@
+//
+//  CDLoginViewController.h
+//  cargoDispatcher
+//
+//  Created by Macbook Air on 8/6/14.
+//  Copyright (c) 2014 Macbook Air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDLoginViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet UITextField *contraseñaTextField;
+@property (weak, nonatomic) IBOutlet UITextField *numeroCuentaTextField;
+
+
+
+@end
---
+++ App/cargoDispatcher/cargoDispatcher/CDLoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  CDLoginViewController.m
+//  cargoDispatcher
+//
+//  Created by Macbook Air on 8/6/14.
+//  Copyright (c) 2014 Macbook Air. All rights reserved.
+//
+
+#import "CDLoginViewController.h"
+
+@interface CDLoginViewController ()
+
+@end
+
+@implementation CDLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
